8b8829ab5b56c6a970f854dc224a3180b2b296eb
AdiRocks007 <adityakumar290304@gmail.com>
2024-06-10 16:24:36 +0530

    layer trial 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { Map } from './components/Map';
-import LeafletLogo from './assets/leafletlogo.png';
-import ReactIcon from './assets/reacticon.png';
-import tru from './assets/truestatelogo.png'
+import React from 'react';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        {/* <img src={LeafletLogo} alt={'leaflet logo'} className="logo-mid"/>
-        <img src={ReactIcon} alt={'react logo'} className="logo-small"/> */}
-        <img src={tru} alt={'tru logo'} className="logo-mid"/>
-      </nav>
       <Map />
     </div>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,63 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import 'leaflet.gridlayer.googlemutant';
+import React, { useEffect, useState, useRef } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import Select from 'react-select';
 
-// Custom icons for different types of markers
-const metroIcon = new L.Icon({
-  iconUrl: '/metro.png',
-  iconSize: [30, 30],
-  iconAnchor: [12, 12],
-  popupAnchor: [0, -10],
-});
+const libraries = ['places', 'geometry']; // Include 'geometry' library
 
-const hospitalIcon = new L.Icon({
-  iconUrl: '/hospital.png',
-  iconSize: [30, 30],
-  iconAnchor: [12, 12],
-  popupAnchor: [0, -10],
-});
+const defaultCenter = { lat: 12.9927655, lng: 77.8060448 };
 
-const schoolIcon = new L.Icon({
-  iconUrl: '/education.png',
-  iconSize: [30, 30],
-  iconAnchor: [12, 12],
-  popupAnchor: [0, -10],
-});
-
-const projectIcon = new L.Icon({
-  iconUrl: '/location-pin.png',
-  iconSize: [30, 30],
-  iconAnchor: [12, 12],
-  popupAnchor: [0, -10],
-});
-
-const AdditionalMarkers = ({ markers }) => (
-  markers.map(marker => (
-    <Marker key={marker.key} position={marker.position} icon={marker.icon}>
-      <Popup>{marker.popupContent}</Popup>
-    </Marker>
-  ))
-);
-
-const ZoomToProject = ({ position }) => {
-  const map = useMap();
-  useEffect(() => {
-    if (position) {
-      map.setView(position, 12);
-    }
-  }, [position, map]);
-  return null;
+const mapContainerStyle = {
+  height: '60vh',
+  width: '100%',
 };
 
-export function Map() {
-  const defaultPosition = [12.9927655, 77.8060448];
+const nightMode = [
+  // Include your night mode styles here
+];
+
+const mapOptions = {
+  disableDefaultUI: true,
+  styles: nightMode,
+};
+
+const options = [
+  { value: 'school', label: 'Schools' },
+  { value: 'hospital', label: 'Hospitals' },
+  { value: 'subway_station', label: 'Metros' },
+  { value: 'restaurant', label: 'Restaurants' },
+  { value: 'cafe', label: 'Cafes' },
+  { value: 'bank', label: 'Banks' },
+  // Add more options as needed
+];
+
+const MAX_RADIUS = 50000; // Maximum radius for nearby search in meters
+
+const Map = () => {
   const [projects, setProjects] = useState([]);
-  const [metroData, setMetroData] = useState(null);
-  const [showMetro, setShowMetro] = useState(false);
-  const [selectedProject, setSelectedProject] = useState(null);
-  const [additionalMarkers, setAdditionalMarkers] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [markers, setMarkers] = useState([]);
+  const [customIcons, setCustomIcons] = useState({});
+  const mapRef = useRef(null);
+  const serviceRef = useRef(null);
 
   useEffect(() => {
     fetch('/updated_file_with_nearby_json.json')
@@ -69,98 +50,160 @@ export function Map() {
       .catch(error => {
         console.error('Error fetching JSON data:', error);
       });
-
-    fetch('/metro-lines-stations.geojson')
-      .then(response => response.json())
-      .then(data => {
-        setMetroData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching GeoJSON data:', error);
-      });
   }, []);
 
-  const handleMarkerClick = (project) => {
-    console.log('Selected project:', project);
-    setSelectedProject(project);
-    const additionalMarkersData = [];
-
-    const processNearbyLocations = (locations, type, icon) => {
-      if (locations && Array.isArray(locations)) {
-        locations.forEach((location, index) => {
-          const [name, distance, coordinates] = Array.isArray(location) ? location : [location.name, location.distance, [location.coordinates.latitude, location.coordinates.longitude]];
-          if (Array.isArray(coordinates) && coordinates.length === 2) {
-            const [latitude, longitude] = coordinates;
-            if (!isNaN(latitude) && !isNaN(longitude)) {
-              additionalMarkersData.push({
-                key: `${type}-${index}`,
-                position: [latitude, longitude],
-                icon: icon,
-                popupContent: `${name} (${distance} km)`,
-              });
-            } else {
-              console.warn(`Invalid ${type} coordinates for ${name}`);
-            }
-          } else {
-            console.warn(`Coordinates object missing for ${type}: ${name}`);
-          }
-        });
-      } else {
-        console.warn(`Nearest ${type} data missing or not an array.`);
-      }
-    };
-
-    processNearbyLocations(project.nearest_metros, 'metro', metroIcon);
-    processNearbyLocations(project.nearest_hospitals, 'hospital', hospitalIcon);
-    processNearbyLocations(project.nearest_schools, 'school', schoolIcon);
-
-    setAdditionalMarkers(additionalMarkersData);
+  const initializeIcons = () => {
+    setCustomIcons({
+      school: {
+        url: '/education.png',
+        scaledSize: new window.google.maps.Size(40, 40),
+      },
+      hospital: {
+        url: '/hospital.png',
+        scaledSize: new window.google.maps.Size(40, 40),
+      },
+      subway_station: {
+        url: '/metro.png',
+        scaledSize: new window.google.maps.Size(40, 40),
+      },
+      restaurant: {
+        url: '/location-pin.png',
+        scaledSize: new window.google.maps.Size(40, 40),
+      },
+      cafe: {
+        url: '/location-pin.png',
+        scaledSize: new window.google.maps.Size(40, 40),
+      },
+      bank: {
+        url: '/location-pin.png',
+        scaledSize: new window.google.maps.Size(40, 40),
+      },
+      // Add more as needed
+    });
   };
 
-  const handleResetClick = () => {
-    setSelectedProject(null);
-    setAdditionalMarkers([]);
+  const handleSelectChange = (selectedOptions) => {
+    const options = selectedOptions ? selectedOptions.map(option => option.value) : [];
+    setSelectedOptions(options);
+    updateMarkers(options);
+  };
+
+  const updateMarkers = (selectedOptions) => {
+    if (serviceRef.current && mapRef.current) {
+      const newMarkers = [];
+
+      selectedOptions.forEach(option => {
+        const request = {
+          location: defaultCenter,
+          radius: MAX_RADIUS,
+          type: option,
+        };
+
+        serviceRef.current.nearbySearch(request, (results, status) => {
+          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+            results.forEach(place => {
+              newMarkers.push({
+                type: option,
+                position: { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() },
+                name: place.name,
+              });
+            });
+
+            // Check for projects outside the initial search radius
+            const outlyingProjects = projects.filter(project => {
+              const distance = window.google.maps.geometry.spherical.computeDistanceBetween(
+                new window.google.maps.LatLng(defaultCenter.lat, defaultCenter.lng),
+                new window.google.maps.LatLng(project.Latitude, project.Longitude)
+              );
+              return distance > 20000;
+            });
+
+            // Perform search for outlying projects
+            outlyingProjects.forEach(project => {
+              const projectRequest = {
+                location: { lat: project.Latitude, lng: project.Longitude },
+                radius: MAX_RADIUS,
+                type: option,
+              };
+
+              serviceRef.current.nearbySearch(projectRequest, (projectResults, projectStatus) => {
+                if (projectStatus === window.google.maps.places.PlacesServiceStatus.OK) {
+                  projectResults.forEach(place => {
+                    newMarkers.push({
+                      type: option,
+                      position: { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() },
+                      name: place.name,
+                    });
+                  });
+                  setMarkers(newMarkers);
+                }
+              });
+            });
+          }
+        });
+      });
+    }
+  };
+
+  const handleRemoveAmenity = (removedOption) => {
+    const updatedMarkers = markers.filter(marker => marker.type !== removedOption.value);
+    setMarkers(updatedMarkers);
+  };
+
+  const handleAmenityChange = (selectedOptions) => {
+    const currentOptions = selectedOptions ? selectedOptions.map(option => option.value) : [];
+    const removedOptions = selectedOptions
+      ? options.filter(option => !currentOptions.includes(option.value))
+      : options;
+
+    removedOptions.forEach(removedOption => handleRemoveAmenity(removedOption));
+    handleSelectChange(selectedOptions);
   };
 
   return (
     <section className="map">
       <div className="container">
-        <MapContainer style={{ height: '60vh', width: '100%' }} center={defaultPosition} zoom={12} scrollWheelZoom={true}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <TileLayer
-            url="https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}"
-            attribution='&copy; <a href="https://www.google.com/maps">Google Maps</a>'
-          />
-          {!selectedProject && projects.map((project, index) => (
-            <Marker
-              key={`project-${index}`}
-              position={[project.Latitude, project.Longitude]}
-              icon={projectIcon}
-              eventHandlers={{ click: () => handleMarkerClick(project) }}
-            >
-              <Popup>{project['Project Name']}</Popup>
-            </Marker>
-          ))}
-          {selectedProject && (
-            <>
-              <ZoomToProject position={[selectedProject.Latitude, selectedProject.Longitude]} />
-              <Marker key={`selected-project`} icon={projectIcon} position={[selectedProject.Latitude, selectedProject.Longitude]}>
-                <Popup>{selectedProject['Project Name']}</Popup>
-              </Marker>
-              <AdditionalMarkers markers={additionalMarkers} />
-            </>
-          )}
-          {showMetro && metroData && <GeoJSON data={metroData} />}
-        </MapContainer>
-        {selectedProject && (
-          <button onClick={handleResetClick} style={{ marginTop: '10px' }}>
-            Reset to Project Markers
-          </button>
-        )}
+        <Select
+          options={options}
+          isMulti
+          onChange={handleAmenityChange}
+          placeholder="Select amenities"
+        />
+        <LoadScript
+          googleMapsApiKey="AIzaSyDzNTkZVr61Ofe6qC7BzEZKh1DASc0ADP4"
+          libraries={libraries}
+          onLoad={initializeIcons} // Initialize custom icons after the script is loaded
+          onError={(error) => console.error('Error loading Google Maps script:', error)}
+        >
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            center={defaultCenter}
+            zoom={12}
+            options={mapOptions}
+            onLoad={map => {
+              mapRef.current = map;
+              serviceRef.current = new window.google.maps.places.PlacesService(map);
+            }}
+          >
+            {projects.map((project, index) => (
+              <Marker
+                key={`project-${index}`}
+                position={{ lat: project.Latitude, lng: project.Longitude }}
+              />
+            ))}
+            {markers.map((marker, index) => (
+              <Marker
+                key={`marker-${index}`}
+                position={marker.position}
+                title={marker.name}
+                icon={customIcons[marker.type]}
+              />
+            ))}
+          </GoogleMap>
+        </LoadScript>
       </div>
     </section>
   );
-}
+};
+
+export default Map;
