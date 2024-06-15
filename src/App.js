e315b7a80f2ca743e966a8c131cfbeba0dd21280
AdiRocks007 <adityakumar290304@gmail.com>
2024-06-15 17:46:14 +0530

    side panel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import Map from './components/Map';
+import React, { useState, useEffect } from 'react';
+import Map from './components/Map/Map';
+import ProjectDetails from './components/ProjectDetails/ProjectDetails';
+import './App.css';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [imageData, setImageData] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  useEffect(() => {
+    fetch('/updated_file_with_nearby_json2.json')
+      .then(response => response.json())
+      .then(data => setProjects(data))
+      .catch(error => console.error('Error fetching JSON data:', error));
+
+    fetch('/output_projects_with_images.json')
+      .then(response => response.json())
+      .then(data => setImageData(data))
+      .catch(error => console.error('Error fetching images data:', error));
+  }, []);
+
+  const handleSelectProject = (project) => {
+    setSelectedProject(project);
+  };
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div className="App">
-      <Map />
+      <div className={`left-half ${isCollapsed ? 'collapsed' : ''}`}>
+        <ProjectDetails
+          projects={projects}
+          selectedProject={selectedProject}
+          onSelectProject={handleSelectProject}
+          imageData={imageData}
+        />
+      </div>
+      <div className={`right-half ${isCollapsed ? 'full' : ''}`}>
+        <Map
+          projects={projects}
+          selectedProject={selectedProject}
+          onSelectProject={handleSelectProject}
+          imageData={imageData}
+        />
+      </div>
+      <button className={`collapse-button ${isCollapsed ? 'desc' : 'asc'}`} onClick={toggleCollapse}>
+        {isCollapsed ? '→' : '←'}
+      </button>
     </div>
   );
 }
